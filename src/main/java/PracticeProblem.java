391a5daee557930872226a79df53058ddf6d188a
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-02-25 18:55:27 +0000

    review 12 complete

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,6 +1,43 @@
 public class PracticeProblem {
-
+	
 	public static void main(String args[]) {
+		System.out.println(countWords("My name is yaseman",'m'));
+		
+	}
+	public static boolean isPalindrome(String word){
+		boolean answer= true;
+		word= word.replaceAll(" ", "");
+		word= word.toLowerCase();
+		for (int i =0; i<word.length();i++){
+			if (word.charAt(i)!=word.charAt(word.length()-i-1)){
+				answer= false;
+			}	
+		}
+		return answer; 
+	}
+	public static int getAge(String[] nameArr, int[] ageArr, String name){
+		
+		int age=-1;
+		for (int i= 0; i <nameArr.length; i++){
+			if (nameArr[i]== name){
+				age=ageArr[i];
+			}
+		}
+		return age;
+	}
+	public static int countWords(String word, char letter){
+		word =word.toLowerCase();
+		String[] wordArr= word.split(" ");
+		int  count=0;
+		for (int i =0; i< wordArr.length;i++){
+			for (int z=0; z<wordArr[i].length();z++){
+				if (wordArr[i].charAt(z)==letter){
+					count++;
+					z=wordArr[i].length();
+				}
+			}
+		}
 
+		return count;
 	}
 }
